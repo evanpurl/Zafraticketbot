14962b4b254a10b606773785154e89f177f8a0a3
evanpurl <evan@purlcomputers.com>
2023-03-13 16:41:18 -0600

    Updated embed

--- cogs/rpsupportticket.py
+++ cogs/rpsupportticket.py
@@ -147,9 +147,7 @@ class ticketbuttonpanel(discord.ui.View):
             rolelist = ['RP Junior Moderator', 'RP Moderator', 'RP Senior ', 'Moderator', 'RP Administrator', 'RP '
                                                                                                               'Staff '
                                                                                                               'Manager']
-            for role in interaction.user.roles:
-                print(role.name)
-            if any(role in rolelist for role in interaction.user.roles):
+            if any(role.name in rolelist for role in interaction.user.roles):
                 await interaction.response.send_message(content=f"Ticket has been claimed by {interaction.user.mention}")
             else:
                 await interaction.response.send_message(content=f"You're not authorized to do that", ephemeral=True)
